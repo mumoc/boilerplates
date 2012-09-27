e55deb91fc58008a58ec4490058807d3cc75a669
Mario Zaizar <mariozaizar@gmail.com>
2012-09-27 12:08:45 -0500

    New app for jasminerice.

---
+++ jasminerice_coffeescript_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasminericeCoffeescriptRails::Application.config.secret_token = '0484983cf29dd7940b3643c91a37d51a76e9673de73881f0659b59d8ac22d20d091f171111d5d38bb207ce7ac06388ceab9b3766e785b64878f9030cdfe50590'
