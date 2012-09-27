77e484b0724eadc0f1b0a352ab929baec24d04f9
Mario Zaizar <mariozaizar@gmail.com>
2012-09-27 10:54:30 -0500

    Adding `rails new jasmine_coffeescript_rails` files

---
+++ jasmine_coffeescript_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasmineCoffeescriptRails::Application.config.secret_token = '615ce74d4264add082bb759063b7b4c9646bcfd0b0b73bf009ece54e3d53e04264f384a992add95ca9241545ea391221c74c6e8f3aa65eccfcadb8bed68e5be1'
