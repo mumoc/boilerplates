d43e60f91d51a2463324d0ffd070ff7872585f10
Mario Zaizar <mariozaizar@gmail.com>
2012-09-27 15:50:29 -0500

    Adding Jasmine-Rails boilerplate.

---
+++ jasmine-rails_coffeescript/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasmineRailsCoffeescript::Application.config.secret_token = '5bfca659b9c249035d5527501d2a526a9f9af8c6557ef1b0d1e3a7bb80ed12cb327339db5b3a6a0c70628ae6f50d7363abf4c65a611b14a5ebf1bf28b57fd80d'
